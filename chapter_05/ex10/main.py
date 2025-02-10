bdcce3d1507c53b31ea7ddb915cf5dcd5b4d9dec
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-02-10 18:49:17 -0500

    chore: [ch05] Added exercise 10

--- chapter_05/ex10/main.py
+++ chapter_05/ex10/main.py
@@ -1,5 +1,7 @@
+from sklearn.datasets import load_wine
+
 """
-Exercise 10. Train an SVM classifier on the wine dataset, which you can load using `sklearn.dataset.load_wine`.
+Exercise 10. Train an SVM classifier on the wine dataset, which you can load using `sklearn.datasets.load_wine`.
 This dataset contains the chemical analyses of 178 wine samples produced by 3 different cultivators:
 the goal is to train a classification model capable of predicting the cultivator based on the wine's
 chemical analysis. Since SVM classifiers are binary classifiers, you will need to use one-versus-all
